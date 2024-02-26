3c5a62bd76940e0a88eacc69e92183a7d8dd88ef
josmar <josmariso28@gmail.com>
2024-02-25 21:15:50 -0300

    3er commit prueba

--- js/script.js
+++ js/script.js
@@ -2,36 +2,40 @@ function calcularPresupuesto() {
     alert("Bienvenido al calculador de presupuesto.");
 
     while (true) {
-        var tipoDeWeb = prompt("¿Qué tipo de página web deseas: Web Estática, Landing Page o Aplicación Web?");
-        var costoBase = 0;
+        let tipoDeWeb = prompt("¿Qué tipo de página web deseas: Web Estática, Landing Page o Aplicación Web?");
+        let costoBase = 0;
 
-        if (tipoDeWeb === "Web Estática") {
-            costoBase = 100;
-        } else if (tipoDeWeb === "Landing Page") {
-            costoBase = 300;
-        } else if (tipoDeWeb === "Aplicación Web") {
-            costoBase = 400;
-        } else {
-            alert("Tipo de página web no válido. Por favor, elige una opción válida.");
-            continue;
+        switch (tipoDeWeb) {
+            case "Web Estática":
+                costoBase = 100;
+                break;
+            case "Landing Page":
+                costoBase = 300;
+                break;
+            case "Aplicación Web":
+                costoBase = 400;
+                break;
+            default:
+                alert("Tipo de página web no válido. Por favor, elige una opción válida.");
+                continue;
         }
 
-        var diasEntrega = parseInt(prompt("¿En cuántos días la quieres?"));
-        var numPaginas = parseInt(prompt("¿Cuántas páginas tendrá el sitio?"));
+        let diasEntrega = parseInt(prompt("¿En cuántos días la quieres?"));
+        let numPaginas = parseInt(prompt("¿Cuántas páginas tendrá el sitio?"));
 
         if (numPaginas > 3) {
             costoBase += 200;
         }
-        var costoTotal = costoBase;
+        let costoTotal = costoBase;
 
         alert("El costo total de tu proyecto es: $" + costoTotal);
 
-        var continuar = prompt("¿Deseas calcular otro presupuesto? (s/n)");
+        let continuar = prompt("¿Deseas calcular otro presupuesto? (s/n)");
         if (continuar.toLowerCase() !== "s") {
             break;
         }
     }   
-} 
-window.onload = function() {
-        calcularPresupuesto();
-    };
+}
+
+calcularPresupuesto()
+
